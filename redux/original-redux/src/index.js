5f17bf4b5349af448efc5845656379c33e101fef
YeongG <yeonggil223@gmail.com>
2020-06-08 07:49:39 +0900

    Clean Code!!!

--- redux/original-redux/src/index.js
+++ redux/original-redux/src/index.js
@@ -4,11 +4,14 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (state = 0,action) => {
-    if(action.type === "ADD") {
-        return ++state;
-    } else if(action.type === "MINUS") {
-        return --state;
+    switch(action.type) {
+        case "ADD" : return ++state;
+        case "MINUS" : return --state;
+        default : return state;
     }
     return state;
 }
@@ -21,5 +24,5 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 
-add.onclick=() => countStore.dispatch({type:"ADD"});
-minus.onclick=() => countStore.dispatch({type:"MINUS"});
+add.onclick=() => countStore.dispatch({type:ADD});
+minus.onclick=() => countStore.dispatch({type:MINUS});
