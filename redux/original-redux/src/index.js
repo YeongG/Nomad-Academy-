bbe209adf115887ec88b0d1b6139667abded75a7
YeongG <yeonggil223@gmail.com>
2020-06-07 23:10:42 +0900

    modifier action!!

--- redux/original-redux/src/index.js
+++ redux/original-redux/src/index.js
@@ -1,13 +1,20 @@
-import { createStore} from 'redux';
+import { createStore} from 'redux'; 
 
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const countModifier = (state = 0) => {
+const countModifier = (state = 0,action) => {
+    if(action.type === "ADD") {
+        return ++state;
+    } else if(action.type === "MINUS") {
+        return --state;
+    }
     return state;
 }
 
 const countStore = createStore(countModifier);
+countStore.dispatch({type:"ADD"})
+countStore.dispatch({type:"ADD"})
+countStore.dispatch({type:"ADD"})
 console.log(countStore.getState());
-  
