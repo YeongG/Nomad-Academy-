9ed685364d8bc6074721b7c4faea96db3d0f8fac
YeongG <yeonggil223@gmail.com>
2020-06-08 20:20:04 +0900

    TypeScriptgit add .!

---
+++ typescript/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function sayHi(name, age, gender) {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+sayHi("영길", 18, "남자");
+//# sourceMappingURL=index.js.map
---
+++ typescript/index.ts
@@ -0,0 +1,7 @@
+
+function sayHi(name:string,age:number,gender:string):void {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+sayHi("영길",18,"남자");
+export {};
